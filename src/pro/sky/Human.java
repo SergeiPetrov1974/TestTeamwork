9cb46ff427a66ad74313e9299f724fcc244ce8fb
Сергей <040620@mail.ru>
2022-05-11 21:49:16 +0300

    made constructor and start main

--- src/pro/sky/Human.java
+++ src/pro/sky/Human.java
@@ -3,4 +3,13 @@ package pro.sky;
 public class Human {
     private String name = null;
     private int age = 0;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void show() {
+        System.out.println(name + " " + age);
+    }
 }
--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -1,4 +1,9 @@
 package pro.sky;
 
 public class Main {
+    public static void main(String[] args) {
+        Human human = new Human("Sergei", 21);
+        human.show();
+    }
+
 }
